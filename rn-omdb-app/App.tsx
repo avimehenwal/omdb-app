c23f79f1898241d44412f720ea4089d7a96067ef
avimehenwal <avi.mehanwal@gmail.com>
2021-11-22 15:29:15 +0100

    feat: :zap: accept user input and 2-way bind it to app-data

--- rn-omdb-app/App.tsx
+++ rn-omdb-app/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
@@ -19,10 +19,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 export const ShowMovies = () => {
   const [movies, setMovies] = React.useState<IMovies | null>(null);
+  const [text, setText] = React.useState("Useless Text");
+
+  const onChangeText = (newText: string) => {
+    setText(newText)
+  }
 
   React.useEffect((): void => {
     getMoviesFromApi().then(items => {
@@ -34,6 +45,12 @@ export const ShowMovies = () => {
 
   return (
     <>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text>{text}</Text>
       <Text>{JSON.stringify(movies, null, 2)}</Text>
     </>
   )

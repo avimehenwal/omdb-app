7fa07c62ac7228ebbe1fa45253b48d09fa06619f
avimehenwal <avi.mehanwal@gmail.com>
2021-12-01 22:33:25 +0100

    refactor: :zap: decouple data from markup

---
+++ web-omdb-app/omdb-app/app-config.ts
@@ -0,0 +1,14 @@
+// Application Level Configurations
+export const config = {
+  head: {
+    title: "OMDB web app",
+    favicon: "/favicon.ico"
+    // TODO:  SEO meta tags
+  },
+  body: {
+
+  },
+  footer: {
+    text: "Some footer text"
+  }
+}
